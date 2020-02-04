84658c5f9c65d6442eb01dce7dfd0dda43040c0d
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-02-04 17:04:06 -0500

    Add collision detection

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ char = " "
 
 game_exit = False
 
+ball_max_speed_x = 0.015
+ball_max_speed_y = 0.03
+
 
 class Player:
     def __init__(self, player_id):
+        self.id = player_id
         self.height = 4
         self.char = "|"
 
-        if player_id == 1:
+        if self.id == 1:
             self.x = 3
-        elif player_id == 2:
+        elif self.id == 2:
             self.x = board_width - 3 - 1
         self.y = board_height // 2 - self.height // 2
 
@@ -28,7 +32,7 @@ class Player:
 
     def check_input(self, player_id, input_key):
         # ASCII table reference: https://www.ascii-code.com/
-        if player_id == 1:
+        if self.id == 1:
             if input_key == ord("w"):
                 if self.y > 1:
                     self.y -= 1
@@ -36,7 +40,7 @@ class Player:
                 if self.y < board_height - self.height - 1:
                     self.y += 1
 
-        if player_id == 2:
+        if self.id == 2:
             if input_key == ord("i"):
                 if self.y > 1:
                     self.y -= 1
@@ -50,16 +54,33 @@ class Ball:
         self.x = board_width // 2
         self.y = board_height // 2
 
-        self.speed_x = random.choice([-0.02, 0.02])
-        self.speed_y = random.uniform(-0.04, 0.04)
+        self.speed_x = random.choice([-ball_max_speed_x, ball_max_speed_x])
+        self.speed_y = random.uniform(-ball_max_speed_y, ball_max_speed_y)
 
     def draw(self):
         self.x += self.speed_x
         self.y += self.speed_y
         screen.addstr(int(self.y), int(self.x), "o")
 
-    def check_collision(self):
-        pass
+    def check_collision_wall(self):
+        if self.y <= 1:
+            self.speed_y = random.uniform(0, ball_max_speed_y)
+        if self.y >= board_height - 1:
+            self.speed_y = random.uniform(-ball_max_speed_y, 0)
+
+        if self.x <= 0 or self.x >= board_width:
+            self.__init__()
+
+    def check_collision_player(self, player):
+        if player.id == 1:
+            if self.x <= player.x:
+                if player.y >= self.y >= player.y + player.height:
+                    self.speed_x = ball_max_speed_x
+
+        elif player.id == 2:
+            if self.x >= player.x:
+                if player.y >= self.y >= player.y + player.height:
+                    self.speed_x = -ball_max_speed_x
 
 
 player1 = Player(1)
@@ -86,6 +107,10 @@ while not game_exit:
         game_exit = True
     player1.check_input(1, key)
     player2.check_input(2, key)
+
+    ball.check_collision_wall()
+    ball.check_collision_player(player1)
+    ball.check_collision_player(player2)
 
     screen.refresh()
 
